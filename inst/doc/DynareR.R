4db82b9978b015659b1593f21212d2e3cae7afa2
Sagiru Mati <smati@smati.com.ng>
2020-06-25 07:40:09 +0000

    version 0.1.0

---
+++ inst/doc/DynareR.R
@@ -0,0 +1,20 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup1, include=FALSE----------------------------------------------------
+library(DynareR)
+
+## ----installation,eval=FALSE--------------------------------------------------
+#  install.packages("DynareR")
+#  
+#            OR
+#  
+#  devtools::install_github('sagirumati/DynareR')
+
+## ----BKKFigure,fig.cap="A sample of figure generated from Dynare software",eval=F----
+#  knitr::include_graphics("BKK/BKK/graphs/BKK_IRF_E_H2.pdf")
+#  
+
